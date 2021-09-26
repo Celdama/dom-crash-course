116bedce49249e83e044ecc1674528daad22f571
Celdama <celdama.dev@gmail.com>
2021-09-26 07:43:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Examine the document object //
+
+// console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all)
+
